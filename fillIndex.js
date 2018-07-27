ca3720402891480990c50d92dda892b394d0aad8
Michael Graf <mi.graf@sap.com>
2018-07-27 20:59:14 +0200

    Small fixes in indexing script

--- fillIndex.js
+++ fillIndex.js
@@ -9,6 +9,8 @@ fs.readFile('./index.json', 'utf8', function (err, data) {
 	if (err) {
 		return console.log(err);
 	}
+
+	// fill fields
 	data = data.replace(/{{namespace}}/g, sNamespace);
 	data = data.replace(/{{icon}}/g, oPackage.ui5lab.icon);
 	data = data.replace(/{{displayName}}/g, oPackage.ui5lab.displayName || oPackage.name);
@@ -19,7 +21,7 @@ fs.readFile('./index.json', 'utf8', function (err, data) {
 	data = data.replace(/{{demo}}/g, oPackage.homepage);
 	data = data.replace(/{{documentation}}/g, oPackage.ui5lab.documentation || oPackage.repository.url.replace("\.git", "") + "/blob/master/README.md");
 
-	console.log(data);
+	// write into namespace folder where the browser expects it
 	fs.writeFile(sLibraryIndex, data, 'utf8', function (err) {
 		if (err) return console.log(err);
 	});
